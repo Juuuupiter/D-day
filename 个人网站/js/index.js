d78719dece983c0ee0cb39eb00d21806e3d13351
Juuuupiter <691107824@qq.com>
2017-10-09 21:11:21 +0800

    个人网站

--- 个人网站/js/index.js
+++ 个人网站/js/index.js
@@ -1,6 +1,7 @@
 /**
  * Created by 李木子 on 2017/10/3.
  */
-$("你的按钮").on("click",function () {
-    $("html,body").animate({scrollTop:$("你要跳到的div").offset().top},多久跳到（毫秒）);
+
+$("#bb").on("click",function () {
+    $("html,body").animate({scrollTop:$("#about-me").offset().top},1000);
 });
