777a3effcf926e242362d25649dac038375b4e28
Juuuupiter <691107824@qq.com>
2017-10-13 20:53:36 +0800

    juuuuupiter

--- 个人网站/js/index.js
+++ 个人网站/js/index.js
@@ -13,13 +13,38 @@ $("#dd").on("click",function () {
 });
 
 
-$("#text")
-    .typetype('Hey')
-    .delay(100)
-    .typetype('\nWelcome to my world!')
-    .delay(100)
-    .typetype("\nThis is my website.")
-    .typetype("\n  ")
-    .backspace(50)
-    .typetype('\n\n\n这是一颗星球叫M902 ♥')
 
+        $("#letter-up")
+        .typetype(
+            'Hey,',
+            {
+              e:0,
+              t:100
+            });
+
+         $("#letter-middle ")
+             .delay(1000)
+             .typetype('Welcome to my world!',
+            {
+                e:0,
+                t:100
+            })
+        .delay(100);
+         $("#letter-down")
+             .delay(3000)
+             .typetype("This is my website.",
+            {
+                e:0,
+                t:100
+            })
+        .backspace(60);
+         $("#letter-middle")
+             .delay(5000)
+             .backspace(60);
+         $("#letter-down")
+             .delay(500)
+              .typetype('这是一颗星球叫M902 ♥',
+            {
+                e:0,
+                t:100
+            });
